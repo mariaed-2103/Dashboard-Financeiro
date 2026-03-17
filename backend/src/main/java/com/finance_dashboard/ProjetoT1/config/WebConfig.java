7b6650ba4d39f77be4dc00a19ac78f8c179b8e59
maria eduarda <mariaedzinattoo@gmail.com>
2026-03-17 15:26:27 -0300

    arrumei o webconfig

--- backend/src/main/java/com/finance_dashboard/ProjetoT1/config/WebConfig.java
+++ backend/src/main/java/com/finance_dashboard/ProjetoT1/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.finance_dashboard.ProjetoT1.config;
 import com.finance_dashboard.ProjetoT1.security.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
         // Aplica o limite apenas nos endpoints sensíveis (Login e Transações)
         registry.addInterceptor(rateLimitInterceptor)
                 .addPathPatterns("/auth/**", "/transactions/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
     }
 }
